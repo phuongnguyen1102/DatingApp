af095bca8de14f5323356185dba4ba9fd250495a
Phuong Nguyen <phuongnguyen991102@gmail.com>
2020-05-14 16:39:25 +0700

    end of section 8

--- DatingApp/Data/DataContext.cs
+++ DatingApp/Data/DataContext.cs
@@ -6,5 +6,6 @@ namespace DatingApp.Data {
         public DataContext (DbContextOptions<DataContext> options) : base (options) { }
         public DbSet<WeatherForecast> Values { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
     }
 }
---
+++ DatingApp/Dtos/UserForDetailedDto.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using DatingApp.Models;
+
+namespace DatingApp.Dtos {
+    public class UserForDetailedDto {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introductions { get; set; }
+        public string LookingFor { get; set; }
+        public string Interest { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public  string PhotoUrl { get; set; }
+        public ICollection<PhotosForDetailedDto> Photos {get; set;}
+    }
+}
